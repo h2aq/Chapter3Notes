fd4a356d5b2e94b81df406f7899129b21229e9d4
h2aq <h2aq@fresnou.org>
2023-03-14 13:59:36 -0700

    initial commit

---
+++ functions.js
@@ -0,0 +1,42 @@
+//FUNCTIOSN YAYYYY
+//step 1 making da fucntion :)
+// function sayHello();
+//call it now
+// sayHello();
+//You can call it multiple timesss
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+//define
+// function printMax() {
+//     let x = 6;
+//     let y = 29;
+//     if (x>y) {
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+//
+// printMax();
+
+// function printMax(x, y) {
+//     if (x>y) {
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+// printMax(8, 27);
+
+function sum(number1, number2) {
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum(51,651);
+sum(41,21);
+
