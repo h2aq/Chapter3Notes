f3ddb33760cc270360288c1edec9cbfac0dd794d
h2aq <h2aq@fresnou.org>
2023-03-14 14:18:42 -0700

    weee

---
+++ FunctionsEX.js
@@ -0,0 +1,9 @@
+//1
+function findAreaOfRectangle(length, width) {
+    console.log(`The area of the rectangle is ${length*width}`);
+}
+findAreaOfRectangle(5,10);
+
+function findAreaOfCircle (radius) {
+
+}
